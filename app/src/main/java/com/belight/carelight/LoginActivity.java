df68cab71d95496ae951e70f1df548d09a72db65
kinesis19 <lordkinesis@naver.com>
2025-05-26 14:52:49 +0900

    :sparkles: Feat: Find PW feature
    shown toast message

--- app/src/main/java/com/belight/carelight/LoginActivity.java
+++ app/src/main/java/com/belight/carelight/LoginActivity.java
@@ -7,8 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView clickableTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,16 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // 비밀번호 찾기 참조하기
+        clickableTextView = findViewById(R.id.tv_find_pw);
+        clickableTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 비밀번호 찾기 텍스트 클릭 되었을 때
+                Toast.makeText(LoginActivity.this, "텍스트뷰가 클릭됨!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
